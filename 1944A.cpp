8729a8b9bf4d37255d745e3edf56465ba09de68a
Shuvo Chakraborty <141842091+Shuvo-Chakraborty@users.noreply.github.com>
2024-03-17 18:16:32 +0600

    Add files via upload

---
+++ 1944A.cpp
@@ -0,0 +1,46 @@
+///OM TAT SAT
+#include <bits/stdc++.h>
+#define all(v) v.begin(),v.end()
+#define int long long
+using namespace std;
+
+void solve()
+{
+    int N, K;
+    cin >> N >> K;
+    int cnt = N;
+    if(K >= N-1)
+    {
+        cout << 1 << endl;
+    }
+    else cout << cnt << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+
+    return 0;
+}
+
+//6
+//2 0
+//2 1
+//4 1
+//5 10
+//5 3
+//4 4
+//
+//
+
+
