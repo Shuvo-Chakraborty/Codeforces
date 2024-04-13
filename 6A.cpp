a6bffdb3f37629dcd246905c4865b623c3ff960a
Shuvo Chakraborty <141842091+Shuvo-Chakraborty@users.noreply.github.com>
2024-04-13 21:47:37 +0600

    Add files via upload

---
+++ 6A.cpp
@@ -0,0 +1,47 @@
+///OM TAT SAT
+//https://codeforces.com/problemset/problem/6/A
+#include <bits/stdc++.h>
+using namespace std;
+#define Shuvo ios_base::sync_with_stdio(false);
+#define Chakraborty cin.tie(NULL);cout.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define int long long
+
+const long long mod = 1e9 + 7;
+
+bool cmp (pair <int, int> p1, pair <int, int> p2) {
+    if (p1.first == p2.first)
+        return (p1.second < p2.second);
+    else
+        return (p1.first > p2.first);
+}
+void solve()
+{
+    int t=4;
+    vector<int>a(t);
+
+    for(int i=0; i<t; i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    if(a[0]+a[1]>a[2] || a[1]+a[2]>a[3])
+    {
+        cout<<"TRIANGLE"<<endl;
+    }
+    else if(a[0]+a[1]==a[2] || a[1]+a[2]==a[3])
+    {
+        cout<<"SEGMENT"<<endl;
+    }
+    else
+    {
+        cout<<"IMPOSSIBLE"<<endl;
+    }
+}
+
+int32_t main()
+{
+    Shuvo Chakraborty
+    solve();
+    
+    return 0;
+}
